803bbeb70e28e763e788ff078352efc3611f2efa
Ola <hollabodey.kilaso@gmail.com>
2023-08-21 21:28:02 +0100

    Made changes to the html and css files

---
+++ Dec22/25/script.js
@@ -0,0 +1,10 @@
+// let tooltip = document.querySelector('.tooltip');
+// let github = document.querySelector('#gitH');
+
+// github.addEventListener('mouseover', () => {
+//     tooltip.style.visibility = 'visible';
+// });
+
+// github.addEventListener('mouseout', () => {
+//     tooltip.style.visibility = 'hidden';
+// });
