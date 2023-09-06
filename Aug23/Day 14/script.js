70b30fee9e3f2ee577b0a7fb6f1b6d5b893fc70c
Ola <hollabodey.kilaso@gmail.com>
2023-09-06 22:36:18 +0100

    Made changes to the html and javascript file

---
+++ Aug23/Day 14/script.js
@@ -0,0 +1,16 @@
+let currentDate = document.querySelector('.currDay');
+
+let date = new Date(),
+currYear = date.getFullYear(),
+currMonth = date.getMonth();
+
+console.log(date);
+
+let months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+function renderCal() {
+    currentDate.innerText = `${months[currMonth]} ${currYear}`
+
+}
+
+renderCal();
