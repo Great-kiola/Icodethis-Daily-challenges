75d54ce56b2b5f49d598a71a6183c85bd712f155
Ola <hollabodey.kilaso@gmail.com>
2023-09-06 22:54:52 +0100

    Made changes to the index and js file

--- Aug23/Day 14/script.js
+++ Aug23/Day 14/script.js
@@ -1,15 +1,25 @@
 let currentDate = document.querySelector('.currDay');
+let daysTag = document.querySelector('.days');
 
 let date = new Date(),
 currYear = date.getFullYear(),
 currMonth = date.getMonth();
 
-console.log(date);
-
 let months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 function renderCal() {
+    let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(); // to get last date of month
+    let liTag = "";
+
+    for (let i = 1; i <= lastDateofMonth; i++) {
+        liTag += `<li>${i}</li>`;   
+        // console.log(i);
+    }
+
+
+
     currentDate.innerText = `${months[currMonth]} ${currYear}`
+    daysTag.innerHTML = liTag;
 }
 
 renderCal();
