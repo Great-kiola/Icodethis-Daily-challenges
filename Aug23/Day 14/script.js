c2e4667d2b71bd5529e59a4f6f1654c7492200b5
Ola <hollabodey.kilaso@gmail.com>
2023-09-11 20:45:26 +0100

    Made changes to the css and js file

--- Aug23/Day 14/script.js
+++ Aug23/Day 14/script.js
@@ -116,8 +116,8 @@ save.onclick = () => {
 
 taskList.addEventListener('click', function(e){
     if (e.target.tagName === 'LI') {
-        e.target.classList.toggle('Checked');
+        e.target.classList.toggle('checked');
     } else if (e.target.tagName === 'SPAN') {
         e.target.parentElement.remove();
     }
-})
+}, false);
