3bce8c694fd75dec4c25ce74b83661e30d9cebae
Ola <hollabodey.kilaso@gmail.com>
2023-09-16 18:45:50 +0100

    Made changes to the js file

--- Aug23/Day 1/script.js
+++ Aug23/Day 1/script.js
@@ -1,9 +1,15 @@
 let body = document.querySelector('body');
 let toggle = document.querySelector('.icon');
+let i = document.querySelector('i');
 
 
 toggle.addEventListener('click', function(e) {
     e.preventDefault();
 
     body.classList.toggle('darkMode');
+
+    if (body.classList.contains('darkMode')) {
+        return i.classList.replace('fa-moon', 'fa-sun');
+    }
+    i.classList.replace('fa-sun', 'fa-moon');
 });
