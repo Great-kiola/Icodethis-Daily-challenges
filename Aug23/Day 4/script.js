f0c4c3561dbc7a11c9673570bd68dfa78b5a8005
Ola <hollabodey.kilaso@gmail.com>
2023-10-12 19:17:10 +0100

    Made changes to the files

---
+++ Aug23/Day 4/script.js
@@ -0,0 +1,6 @@
+let download = document.querySelector('.download');
+let container = document.querySelector('.container');
+
+download.addEventListener( 'click', function(){
+    container.classList.toggle("remove");
+});
