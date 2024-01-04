ec1c002f0e4c8212b42fd44a7dc369d849a9bfaf
Ola <Hollabodey.kilaso@gmail.com>
2024-01-04 10:53:45 +0000

    Add function to remove div

--- July 2023/Day 4/script.js
+++ July 2023/Day 4/script.js
@@ -2,24 +2,28 @@ let add = document.getElementById('add'),
  remove = document.getElementById('remove');
 
 let quantity = document.querySelector(".quantity");
-
+let orderDiv = document.getElementById('order1');
+let cancel = document.getElementById('cancel');
 
 function increment (){
     add.addEventListener('click', function(){
-        let newVal = quantity.innerHTML++
+        quantity.innerHTML++
     })
 }
 
 function decrement(){
     remove.addEventListener('click', function(){
-        let newVal = quantity.innerHTML--
-
-        // if (newVal > 0 ){
-        //     alert("Can't go below");
-        // }
+        quantity.innerHTML--
     })
+}
+
+function removeDiv(){
+    cancel.addEventListener('click', function(){
+        orderDiv.remove();
+    });
 }
 
 
 increment();
 decrement();
+removeDiv();
